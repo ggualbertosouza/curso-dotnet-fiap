0416a7b0497ed7047ef9bd6cba5ca4ff607d8471
Guilherme Gualberto Souza <guilherme.souza@cacaushow.com.br>
2026-08-17 22:53:36 -0300

    add: explicação inicial

--- Curso.Api/Program.cs
+++ Curso.Api/Program.cs
@@ -1,17 +1,52 @@
+// Cria o builder da nossa aplicação
+// responsável por preparar nossa aplicação, vamos encadear configurações aqui como:
+// 1.Serviços 2.Ambiente 3.Logging 4.Servidores
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// builder.Services é o conjunto de serviçps registrados na aplicação.
 
+// Adiciona as controllers a aplicação
 builder.Services.AddControllers();
 
+// Permite ao ASP.NET Core explorar e descrever os endpoints
+builder.Services.AddEndpointsApiExplorer();
+
+// Gera documentação Swagger/OpenAPI
+builder.Services.AddSwaggerGen();
+
+// Constrói a aplicação - Onde tudo que definimos acima é construído
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+// Definimos o utilizar da interface do swagger no ambiente de desenvolvimento
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
 
+// Adiciona um middleware que redireciona requisições HTTP para HTTPS
 app.UseHttpsRedirection();
 
+// Trata a autenticação - Vamos específicar mais depois
 app.UseAuthorization();
 
+// Reconhece os endpoints da aplicação
 app.MapControllers();
 
+// Efetivamente rodamos a aplicação
 app.Run();
+
+
+/*
+   Cria o builder
+         ↓
+ Configura Serviços
+         ↓
+ Contrói a aplicação
+         ↓
+ Configura Middleware
+         ↓
+  Mapeia Endpoints
+         ↓
+    Executa Api
+ */
